790cb3f24f48b2d66e9ab9b4feecb90bbbe3f819
Leah Xiao <leahxiao@fb.com>
2018-06-26 13:29:13 -0700

    Initial Commit

---
+++ Tipster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tipster
+//
+//  Created by Leah Xiao on 6/26/18.
+//  Copyright © 2018 Leah Xiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
